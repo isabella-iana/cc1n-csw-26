b8e382b16d94450d0a01c485ae4a331457c895ce
isabella-iana <isabellavilareal02@gmail.com>
2026-06-09 21:09:19 -0300

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,12 @@
+function exibirDados()
+{
+
+
+
+    document.getElementById("resposta").innerHTML ="<h1>Olá, Mundo! Esse é meu primeiro código em JavaScript.</h1>" ;  
+
+    document.getElementById("resposta2").value = 'O usuário clicou em : '; + window.confirm("Você está gostando de aprender JavaScript?");
+
+}
+
+
